572a505dd727efad4cabcaee0f4c7e186652aeab
meheraz1100 <meheraz1100@gmail.com>
2024-04-11 19:50:14 +0600

    module practice setup

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/about",
+    element: <div>i am in the About page</div>,
+  },{
+    path: "/contact",
+    element: <div>I am in the contact page</div>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
