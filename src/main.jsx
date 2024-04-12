5f05db59b3ad607b1f8a5962daf4182146e24443
meheraz1100 <meheraz1100@gmail.com>
2024-04-12 18:56:48 +0600

    router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/about",
+        element: <About></About>
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>
+      }
+    ]
   },
-  {
-    path: "/about",
-    element: <div>i am in the About page</div>,
-  },{
-    path: "/contact",
-    element: <div>I am in the contact page</div>
-  }
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
