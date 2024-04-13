1cc954cf484fc3a5f1520584672a244c5a812c87
meheraz1100 <meheraz1100@gmail.com>
2024-04-13 08:24:34 +0600

    create home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom"
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Home = () => {
     return (
         <div>
-            <h2>This is the home component</h2>
+            <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
