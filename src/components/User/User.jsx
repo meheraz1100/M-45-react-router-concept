a282be53245bbe349e0d1667515a2075de0e2dc0
meheraz1100 <meheraz1100@gmail.com>
2024-04-13 08:25:15 +0600

    fetch users data from api

---
+++ src/components/User/User.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const User = ({user}) => {
+    const { id, name, email, phone} = user;
+    const userStyle = {
+        border: '2px solid yellow',
+        padding: '5px',
+        margin: '5px',
+        borderRadius: '20px'
+    }
+    return (
+        <div style={userStyle}>
+            <h2>{name}</h2>
+            <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
+            {/* <Link to={`/user/${id}`}>Show Details</Link> */}
+            <Link to={`/user/${id}`}>
+                <button>Show User Details</button>
+            </Link>
+        </div>
+    );
+};
+ 
+User.propTypes = {
+    id: PropTypes.number,
+    name: PropTypes.string,
+    email: PropTypes.string,
+    phone: PropTypes.string,
+}
+
+export default User;
